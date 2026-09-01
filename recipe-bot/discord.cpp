952edea272b77f2ca230bccabc18b8165c889937
BalatroAP <donovanxabad@gmail.com>
2026-09-01 09:50:57 -0700

    deleted old work

--- recipe-bot/discord.cpp
+++ recipe-bot/discord.cpp
@@ -20,12 +20,12 @@ int main(void) {
               .set_title("SOME RECIPE")
               .set_url("https://youtube.com")
               .set_description("This is some recipe")
-              .set_thumbnail("https://www.startpage.com/av/"
-                             "proxy-image?piurl=https%3A%2F%2Fi.kym-cdn.com%"
-                             "2Fentries%2Ficons%2Foriginal%2F000%2F051%2F089%"
-                             "2Fcarmy_screaming.jpg&sp="
-                             "1788271310Tc3fc12caa92fcfd643c5e7f0303a9e278237ad"
-                             "d5586de26a361086c14f26caaf");
+              .set_image("https://www.startpage.com/av/"
+                         "proxy-image?piurl=https%3A%2F%2Fi.kym-cdn.com%"
+                         "2Fentries%2Ficons%2Foriginal%2F000%2F051%2F089%"
+                         "2Fcarmy_screaming.jpg&sp="
+                         "1788271310Tc3fc12caa92fcfd643c5e7f0303a9e278237ad"
+                         "d5586de26a361086c14f26caaf");
 
       dpp::message msg(event.command.channel_id, embed);
 
